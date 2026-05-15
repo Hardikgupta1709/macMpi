57245e9b39fa3d016ea755e5f31de26110a313bb
Hardik <hardikgupta506@gmail.com>
2026-05-15 12:22:11 +0530

    Implemented Socketpair, for full-duplex UNIX domain socket mesh

--- src/mpirun.c
+++ src/mpirun.c
@@ -5,6 +5,7 @@
 #include <sys/wait.h>
 #include <signal.h> // For signal handling
 #include <string.h>
+#include <sys/socket.h>
 
 // Global variables so that the singal handler can access
 pid_t *child_pids = NULL;
@@ -71,6 +72,35 @@ int main(int argc, char *argv[])
     struct pollfd pollfds[num_procs];
     int active_pipes = num_procs;
 
+    int *socket_mesh = calloc(num_procs * num_procs, sizeof(int));
+    if (socket_mesh == NULL)
+    {
+        fprintf(stderr, "[mpirun] Fatal: Out of memory allocating socket mesh.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("[mpirun] Establishing Unix Domain Socket mesh...\n");
+
+    for (int i = 0; i < num_procs; i++)
+    {
+        for (int j = i + 1; j < num_procs; j++)
+        {
+            int pair[2];
+
+            if (socketpair(AF_UNIX, SOCK_STREAM, 0, pair) < 0)
+            {
+                perror("[mpirun] Fatal: socketpair failed");
+                free(socket_mesh);
+                exit(EXIT_FAILURE);
+            }
+
+            socket_mesh[i * num_procs + j] = pair[0];
+            socket_mesh[j * num_procs + i] = pair[1];
+        }
+    }
+
+    printf("[mpirun] Mesh topology successfully created!\n");
+
     for (int i = 0; i < num_procs; i++)
     {
         if (pipe(pipes[i]) == -1)
@@ -97,8 +127,53 @@ int main(int argc, char *argv[])
             sprintf(rank_str, "%d", i);
             setenv("MPI_RANK", rank_str, 1);
 
+            // MAX int is 10 chars + 1 for comma + 1 for null terminator = 12 bytes per rank
+            size_t fd_list_size = num_procs * 12;
+            char *fd_list = malloc(fd_list_size);
+            if (fd_list == NULL)
+            {
+                perror("[mpirun-chidl] Fatal: malloc failed for fd_list");
+                exit(EXIT_FAILURE);
+            }
+            fd_list[0] = '\0';
+            int my_rank = i;
+
+            for (int r1 = 0; r1 < num_procs; r1++)
+            {
+                for (int r2 = r1 + 1; r2 < num_procs; r2++)
+                {
+                    int handset_A = socket_mesh[r1 * num_procs + r2];
+                    int handset_B = socket_mesh[r2 * num_procs + r1];
+
+                    if (my_rank == r1)
+                    {
+                        close(handset_B);
+                        char temp[16];
+                        snprintf(temp, sizeof(temp), "%d,", handset_A);
+                        strcat(fd_list, temp);
+                    }
+                    else if (my_rank == r2)
+                    {
+                        close(handset_A);
+                        char temp[16];
+                        snprintf(temp, sizeof(temp), "%d,", handset_B);
+                        strcat(fd_list, temp);
+                    }
+                    else
+                    {
+                        close(handset_A);
+                        close(handset_B);
+                    }
+                }
+            }
+
+            setenv("MPI_SOCKET_FDS", fd_list, 1);
+
             execlp(target_executable, target_executable, NULL);
             perror("exec failed");
+
+            free(fd_list);
+            free(socket_mesh);
 
             exit(1);
         }
@@ -116,6 +191,18 @@ int main(int argc, char *argv[])
 
     printf("mpirun: All processses spawned. Listening for output...\n");
     char buffer[1024];
+
+    // The parent doesn't participate in MPI messages
+    for (int r1 = 0; r1 < num_procs; r1++)
+    {
+        for (int r2 = r1 + 1; r2 < num_procs; r2++)
+        {
+            close(socket_mesh[r1 * num_procs + r2]);
+            close(socket_mesh[r2 * num_procs + r1]);
+        }
+    }
+
+    free(socket_mesh);
 
     // polling till we have acitve pipes open
     while (active_pipes > 0)
